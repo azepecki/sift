aa59d0d975cc6e2b1ca6b764968dc7996621ee81
lamarashed <la.alrashed1@gmail.com>
2022-03-28 00:17:39 -0400

    ast.ml and sast.ml first push

---
+++ sast.ml
@@ -0,0 +1,104 @@
+(* Semantically-checked  Abstract Syntax Tree and functions for printing it *)
+open Ast
+
+type sbind = typ * string
+
+type sexpr =
+  SLiteral of int
+| SFLit of string
+| SBoolLit of bool
+| SCharLit of char
+| SStrLit of string
+| SSymLit of string
+| SArrayLit of sexpr list
+| SArrAssign of string * sexpr * sexpr
+| SArrLength of sexpr
+| SArrayAccess of string * sexpr
+| SId of string
+| SBinop of sexpr * op * sexpr
+| SSeq of sexpr * sexpr
+| SAssign of string * sexpr
+| SDeclAssn of typ * string * sexpr
+| SCall of string * sexpr list
+| SNoexpr
+
+
+type sstmt =
+  SBlock of sstmt list
+| Expr of expr
+| If of expr * sstmt * sstmt
+| For of expr * expr * expr * sstmt
+| While of expr * sstmt
+| Return of sstmt
+| Continue of sstmt
+
+
+(* func_def: ret_typ fname formals locals body *)
+type sfunc_def = {
+  srtyp: typ;
+  sfname: string;
+  sformals: bind list;
+  slocals: bind list;
+  sbody: stmt list;
+}
+
+type sprogram = sstmt list * sbind list * sfunc_decl list
+
+
+
+(* Pretty-printing functions *)
+
+
+let rec string_of_sexpr (t,e)= 
+  "(" ^ string_of_typ t ^ " : " ^ ( match w with 
+    SLiteral(l) -> string_of_int l
+  | SFLit(l) -> l
+  | SBoolLit(true) -> "true"
+  | SBoolLit(false) -> "false"
+  | SCharLit(c) -> c
+  | SStrLit(s) -> s
+  | SSymLit(s) -> s
+  | SArrayDecl(t, s, e) -> "arr" ^ "<" string_of_typ t ^ ">" ^ " " ^  s ^ " = " ^ "[" ^ String.concat "," (List.map string_of_expr (List.rev e)) ^ "]"
+  | SArrayLit(e) -> "[" ^ String.concat "," (List.map string_of_sexpr (List.rev e)) ^ "]"
+  | SArrayAccess (s, e) ->  s ^ "[" ^ string_of_sexpr e ^ "]"
+  | SArrAssign(s, e1, e2) -> s ^ "[" ^ string_of_sexpr e1 ^ "] = " ^ string_of_sexpr  e2
+  | SId(s) -> s
+  | SBinop(e1, o, e2) ->
+      string_of_sexpr e1 ^ " " ^ string_of_op o ^ " " ^ string_of_sexpr e2
+  | SAssign(v, e) -> v ^ " = " ^ string_of_sexpr e
+  | SDeclAssn(t, s, e) -> string_of_typ t ^ " " ^ s ^ " = " ^ string_of_sexpr e 
+  | SCall(f, el) ->
+      f ^ "(" ^ String.concat ", " (List.map string_of_sexpr el) ^ ")"
+  | SIncrement(v, e) -> v ^ "+= " ^ string_of_sexpr e
+  | SDecrement(v, e) -> v ^ "-="  ^ string_of_sexpr e 
+  | SNoexpr -> ""
+  ) ^ ")"
+
+let rec string_of_sstmt = function
+    SBlock(stmts) ->
+      "{\n" ^ String.concat "" (List.map string_of_sstmt stmts) ^ "}\n"
+  | SExpr(expr) -> string_of_sexpr expr ^ ";\n";
+  | SReturn(expr) -> "return " ^ string_of_sexpr expr ^ ";\n";
+  | SIf(e, s, Block([])) -> "if (" ^ string_of_sexpr e ^ ")" ^ "{" ^ string_of_sstmt s ^ "};"
+  | SIf(e, s1, s2) ->  "if (" ^ string_of_sexpr e ^ ")" ^ "{" ^
+  string_of_sstmt s1 ^ "}" ^ "else" ^ "{" ^ string_of_sstmt s2 ^ "};"
+  | SFor(e1, e2, e3, s) ->
+      "for (" ^ string_of_sexpr e1  ^ " ; " ^ string_of_sexpr e2 ^ " ; " ^
+      string_of_sexpr e3  ^ ") " ^ "{" string_of_sstmt s ^ "};"
+  | SWhile(e, s) -> "while (" ^ string_of_sexpr e ^ ") " ^ "{" ^ string_of_sstmt s ^ "};"
+  | SContiune(s) -> string_of_sstmt  s
+  | SBreak(s) ->  string_of_sstmt  s
+
+
+let string_of_sfdecl fdecl =
+  string_of_typ fdecl.srtyp ^ " " ^
+  fdecl.sfname ^ "(" ^ String.concat ", " (List.map snd fdecl.sformals) ^
+  ")\n{\n" ^
+  String.concat "" (List.map string_of_vdecl fdecl.slocals) ^
+  String.concat "" (List.map string_of_sstmt fdecl.sbody) ^
+  "}\n"
+
+let string_of_sprogram (vars, funcs) =
+  "\n\nParsed program: \n\n" ^
+  String.concat "" (List.map string_of_vdecl vars) ^ "\n" ^
+  String.concat "\n" (List.map string_of_sfdecl funcs)
