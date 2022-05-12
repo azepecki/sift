54bc07d09cd3d9599af163ad4527185c87420773
Rishav Kumar <rishav.kumar@columbia.edu>
2022-05-11 22:49:13 -0400

    Adding new c binaries in test suite

--- generate.sh
+++ generate.sh
@@ -46,7 +46,7 @@ Generate() {
 
     Run "$SIFT" "./$1" ">" "${basename}.ll" &&
     Run "$LLC" "-relocation-model=pic" "${basename}.ll" ">" "${basename}.s" &&
-    Run "$CC" "-o" "${basename}.exe" "${basename}.s" "sift_func.o" &&
+    Run "$CC" "-o" "${basename}.exe" "${basename}.s" "sift_func.o" "similarity.o" "regex.o" &&
     Run "./${basename}.exe" > "${basename}.out" &&
     Compare ${basename}.out ${reffile}.out ${basename}.diff
 }
