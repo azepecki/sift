be63e73aac2c3be8e260a817c05a9e1b8be6e066
jar2333 <jar2333@columbia.edu>
2022-05-11 20:44:04 -0400

    fixed test syntax

--- src/run.ml
+++
@@ -1,9 +0,0 @@
-open Ast
-open Sast
-open Semantics
-
-
-let (v, f) = Parser.program Scanner.token (Lexing.from_channel stdin);;
-
-print_endline (Ast.string_of_program (v, f));;
-print_endline (Sast.string_of_sprogram (check_program v f));;
